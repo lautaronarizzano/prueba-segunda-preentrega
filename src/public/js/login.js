3eab6faed47692ddad9ad0eac87eaf9e3e00c018
Lautaro Narizzano <lautaronarizzano@gmail.com>
2023-04-09 17:02:32 -0300

    agregar prueba

---
+++ src/public/js/login.js
@@ -0,0 +1,41 @@
+const form = document.getElementById('loginForm')
+
+form.addEventListener('submit', async e => {
+    e.preventDefault()
+    console.log('hola')
+
+    // const data  = new FormData(form)
+    // const obj = {}
+
+    // data.forEach((value, key) => obj[key] = value)
+
+    // fetch('/api/sessions/login', {
+    //     method: 'POST',
+    //     body: JSON.stringify(obj),
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // }).then(result => {
+    //     if(result.status === 200) {
+    //         window.location.replace('/products')
+    //     }
+    // })
+
+    const datos = {
+        email: form[0].value,
+        password: form[1].value,
+    }
+
+    const respuesta = await fetch('/api/auth/login', {
+        method: 'POST',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(datos)
+    });
+
+    if(respuesta.status === 200) {
+        console.log(document.cookie)
+    } 
+})
---
+++ src/routes/api/sessions.router.js
@@ -0,0 +1,86 @@
+import {
+    Router
+} from 'express'
+import userModel from '../../dao/models/usersModel.js'
+import passport from 'passport';
+import {
+    generateToken,
+    isValidPassword
+} from '../../utils.js';
+
+const router = Router()
+
+router.post('/register', passport.authenticate('register', {
+    failureRedirect: 'fail-register'
+}), async (req, res) => {
+    res.send({
+        status: 'success',
+        message: 'user Registered'
+    })
+})
+
+router.get('/fail-register', async (req, res) => {
+    res.send({
+        status: 'error',
+        message: 'register failed'
+    })
+})
+
+
+router.post('/login', async (req, res) => {
+    const {
+        email,
+        password
+    } = req.body
+
+    try {
+        const user = await userModel.findOne(email)
+
+        if (!user) {
+            return res.status(404).send('User not found')
+        }
+
+        if (!isValidPassword(user, password)) return res.status(400).send('Incorrect credentials')
+
+        const accessToken = generateToken(user)
+
+        res.cookie('cookieToken', accessToken, { maxAge: 60 * 60 * 1000, httpOnly: true })
+        .send({ status: 'success', message: 'login success' });
+    } catch (error) {
+        res.status(400).send({error: error})
+    }
+});
+
+
+
+router.get('/logout', (req, res) => {
+    req.session.destroy(err => {
+        if (err) return res.status(500).send({
+            status: 'error',
+            error: "couldn't logout"
+        })
+        res.redirect('/login')
+    })
+})
+
+router.get('/current', passport.authenticate("jwt", { session: false }), (req, res) => {
+    res.status(200).send({ status: 'success', payload: req.user})
+})
+
+router.get('/github', passport.authenticate('github', {
+    scope: ['user:email']
+}), async (req, res) => {
+    res.send({
+        status: 'success',
+        message: 'user registered'
+    })
+})
+
+router.get('/github-callback', passport.authenticate('github', {
+    failureRedirect: '/login'
+}), (req, res) => {
+    req.session.user = req.user
+    res.redirect('/products')
+})
+
+export default router
